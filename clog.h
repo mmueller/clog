5c23232e7a56515d2e323c4603b605c558179388
Mike Mueller <mike@subfocal.net>
2013-10-11 23:40:43 -0700

    Issue #1: Fix mode parameter to open().
    Instead of using constants which may or may not be defined, just use 0666.
    The process's umask will be applied in the open() call, which on most systems
    will produce 0644.

--- clog.h
+++ clog.h
@@ -279,8 +279,7 @@ const char *const CLOG_LEVEL_NAMES[] = {
 int
 clog_init_path(int id, const char *const path)
 {
-    int fd = open(path, O_CREAT | O_WRONLY | O_APPEND,
-                  S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    int fd = open(path, O_CREAT | O_WRONLY | O_APPEND, 0666);
     if (fd == -1) {
         _clog_err("Unable to open %s: %s\n", path, strerror(errno));
         return 1;
